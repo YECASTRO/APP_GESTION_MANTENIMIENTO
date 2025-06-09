56b72308ccec87b71d2838e9718b3f899026551d
fuenmayore <fuenmayoredwin311@gmail.com>
2025-06-08 19:49:01 -0500

    Organization Code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
 let data_equipo = [];
 let data_status = [[5, "Equipos activos", "Cantidad de equipos registrados en el sistema", "https://cdn-icons-png.flaticon.com/512/2210/2210210.png", "card-morado"], [2, "Mantenimientos pendientes", "Cantidad de notificaciones de mantenimientos sin gestionar", "https://cdn-icons-png.flaticon.com/512/2210/2210147.png", "card-naranja"], [6, "Dispositivos IoT activos", "Cantidad de equipos con IoT vinculados al sistema", "https://cdn-icons-png.flaticon.com/512/2210/2210215.png", "card-azul-light"]];
 let data_prox_man = [["Equipo A", "May 15, 2025", "Preventivo", "Inspección", "https://cdn-icons-png.flaticon.com/512/2210/2210175.png"], ["Equipo B", "May 5, 2025", "Correctivo", "Inspección", "https://cdn-icons-png.flaticon.com/512/2210/2210217.png"], ["Equipo C", "May 10, 2025", "Preventivo", "Inspección", "https://cdn-icons-png.flaticon.com/128/2210/2210163.png"], ["Equipo D", "May 9, 2025", "Correctivo", "Inspección", "https://cdn-icons-png.flaticon.com/512/2210/2210202.png"]];
-let data_equipos =[
-    ["May 5, 2025","Equipo A", "SE2312", "Modelo 123", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-azul"],
-    ["May 10, 2025","Equipo B", "SE2452", "Modelo 234", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-verde-light"],
-    ["May 15, 2025", "Equipo C", "SE2678", "Modelo 456", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-rojo"],
-    ["May 20, 2025", "Equipo D", "SE2890", "Modelo 789", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-morado"],
-    ["May 25, 2025", "Equipo E", "SE3012", "Modelo 012", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-azul-light"],
-    ["May 30, 2025", "Equipo F", "SE3234", "Modelo 345", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-verde-dark"],
-    ["June 5, 2025", "Equipo G", "SE3456", "Modelo 678", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-azul"],
-    ["June 10, 2025", "Equipo H", "SE3678", "Modelo 901", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-rojo"],
-    ["June 15, 2025", "Equipo I", "SE3890", "Modelo 234", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-morado"],
-    ["June 20, 2025", "Equipo J", "SE4012", "Modelo 567", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-verde-light"],
-    ["June 25, 2025", "Equipo K", "SE4234", "Modelo 890", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-azul-light"],
-    ["June 30, 2025", "Equipo L", "SE4456", "Modelo 123", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-verde-dark"],
-    ["July 5, 2025", "Equipo M", "SE4678", "Modelo 456", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-rojo"],
-    ["July 10, 2025", "Equipo N", "SE4890", "Modelo 789", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-morado"],
-    ["July 15, 2025", "Equipo O", "SE5012", "Modelo 012", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-azul"], 
-    ["July 20, 2025", "Equipo P", "SE5234", "Modelo 345", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-verde-light"],
-    ["July 25, 2025", "Equipo Q", "SE5456", "Modelo 678", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-azul-light"],
-    ["July 30, 2025", "Equipo R", "SE5678", "Modelo 901", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-verde-dark"],
-    ["August 5, 2025", "Equipo S", "SE5890", "Modelo 234", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-rojo"],
-    ["August 10, 2025", "Equipo T", "SE6012", "Modelo 567", "https://cdn-icons-png.flaticon.com/128/2210/2210178.png", "card-morado"]
-];
 
 llenarStatusMantenimiento();
 llenarProxMantenimiento();
